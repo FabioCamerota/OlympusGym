01e382fd2c14f8cc8c6ba3362a916047aba6f307
Fabio Camerota <fabiocamerota99@gmail.com>
2021-09-02 07:18:02 +0200

    Myreservations completato

--- app/controllers/myreservations_controller.rb
+++ app/controllers/myreservations_controller.rb
@@ -1,7 +1,15 @@
 class MyreservationsController < ApplicationController
+  #Da qui
+  attr_accessor :courses
+  def initialize
+    @courses = []
+  end
+  #fino a qui. Serve per poter fare l'append su courses. Vedi https://stackoverflow.com/questions/19100463/why-cant-i-append-an-array/19100484#19100484
   def index
-    @reservations = Reservation.where(:user_id => current_user.id)
-    @courses = Course.all #Da eliminare
-    #@courses = Course.where(@reservations.contains()) #DA FINIRE !!!
+    reservations = Reservation.where(:user_id => current_user.id)
+    for reservation in reservations
+      c = Course.find(reservation.course_id)
+      @courses.append(c)
+    end
   end
 end
